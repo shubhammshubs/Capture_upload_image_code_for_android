0cf2c40fbecbd2dea825b6bff3a46bb22b0a1a16
shubhammshubs <shubhammahajan500@gmail.com>
2023-08-21 15:15:58 +0530

    Initial commit

---
+++ app/src/main/java/trif/trifrnd/capture/MainActivity.java
@@ -0,0 +1,103 @@
+package trif.trifrnd.capture;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Define the pic id
+    private static final int pic_id = 123;
+
+    // One Button
+    Button BSelectImage,camera_open_id;
+
+    // One Preview Image
+    ImageView IVPreviewImage;
+
+    // constant to compare
+    // the activity result code
+    int SELECT_PICTURE = 200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // By ID we can get each component which id is assigned in XML file get Buttons and imageview.
+        camera_open_id = findViewById(R.id.captureButton);
+
+        // register the UI widgets with their appropriate IDs
+        BSelectImage = findViewById(R.id.uploadButton);
+        IVPreviewImage = findViewById(R.id.imageView);
+
+        // Camera_open button is for open the camera and add the setOnClickListener in this button
+        camera_open_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the camera_intent ACTION_IMAGE_CAPTURE it will open the camera for capture the image
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                // Start the activity with camera_intent, and request pic id
+                startActivityForResult(camera_intent, pic_id);
+
+            }
+        });
+
+        // handle the Choose Image button to trigger
+        // the image chooser function
+        BSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+        });
+
+
+    }
+
+    // this function is triggered when
+    // the Select Image Button is clicked
+    void imageChooser() {
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+
+//     This method will help to retrieve the image
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Match the request 'pic id with requestCode
+        if (requestCode == pic_id) {
+            // BitMap is data structure of image file which store the image in memory
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            // Set the image in imageview for display
+            IVPreviewImage.setImageBitmap(photo);
+        } else if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    IVPreviewImage.setImageURI(selectedImageUri);
+                }
+            }
+        }
+    }}
+
